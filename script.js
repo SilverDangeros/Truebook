f90f0647c0d5ac4638faaec416c644f565aa923b
SilverDangeros <36893670+SilverDangeros@users.noreply.github.com>
2018-05-04 11:55:55 +0800

    Add files via upload
    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var alert
+alert('Javascript attached succesfully');
+
+function myFunction()
+{
+    var str1 = "Hello ";
+    var str2 = "world!";
+    var res = str1.concat(str2);
+    document.getElementById("demo").innerHTML = res;
+}
